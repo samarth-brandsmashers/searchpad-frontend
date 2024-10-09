351a71fa6cd98c1df64a8952ea2323ad20f90d94
Samarth Jain <samarthj0612@gmail.com>
2024-10-09 23:57:51 +0530

    Harsh - Voice over reel creator section layout

---
+++ app/tools/voice-over/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VoiceOver = () => {
+  return (
+    <div>Voice Over</div>
+  )
+}
+
+export default VoiceOver
---
+++ app/tools/voice-over/reel-creator/page.jsx
@@ -0,0 +1,398 @@
+import React from 'react'
+import "@/styles/tools/voice-over/reelCreator.css"
+
+const page = () => {
+  return (
+    <div className="container">
+      <div className="container2">
+        <div className="heading">
+          <h3>Create stunning graphic </h3>
+          <h3>designs effortlessly with AI </h3>
+          <p>
+            Quickly craft designs that captivate and communicate your vision
+            clearly.
+          </p>
+        </div>
+
+        <div className="dataSection">
+          {/* Section 1 */}
+          <div className="sect1">
+            <p> Marketing & Promotional Graphics</p>
+
+            {/* sec1-di1 */}
+            <div className="sect1-div1">
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box10"></div>
+                  <p>Blank Layout</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box10"></div>
+                  <p>Flyers</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box10"></div>
+                  <p>Brochures</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box10"></div>
+                  <p>Banners</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box10"></div>
+                  <p>Social Media Posts</p>
+                </div>
+              </div>
+            </div>
+
+            {/* Section1- div2 */}
+
+            <div className="sect1-div2">
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box10"></div>
+                  <p>Advertisements</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box10"></div>
+                  <p>Email Campaign</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box10"></div>
+                  <p>Event Invitations</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box10"></div>
+                  <p>Coupons or Vouchers</p>
+                </div>
+              </div>
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box10"></div>
+                  <p>Blank Layout</p>
+                </div>
+              </div>
+            </div>
+            {/* Section 2 */}
+          </div>
+
+          {/* Section2 */}
+          <div className="sect2">
+            <p>Branding & Corporate Identity Graphics</p>
+
+            {/* sec1-di1 */}
+            <div className="sect2-div1">
+              <div className="sect2-div1-box1">
+                <div className="sect2-div1-box1-data">
+                  <div className="box11"></div>
+                  <p>Business Cards</p>
+                </div>
+              </div>
+
+              <div className="sect2-div1-box1">
+                <div className="sect2-div1-box1-data">
+                  <div className="box11"></div>
+                  <p>Letterheads</p>
+                </div>
+              </div>
+
+              <div className="sect2-div1-box1">
+                <div className="sect2-div1-box1-data">
+                  <div className="box11"></div>
+                  <p>Employee ID Cards</p>
+                </div>
+              </div>
+
+              <div className="sect2-div1-box1">
+                <div className="sect2-div1-box1-data">
+                  <div className="box11"></div>
+                  <p>Press Kits</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* ------------------------------------------------------------- */}
+          {/* Section3 */}
+          <div className="sect2">
+            <p>Social Media & Digital Content Graphics</p>
+
+            {/* sec1-di1 */}
+            <div className="sect2-div1">
+              <div className="sect2-div1-box1">
+                <div className="sect2-div1-box1-data">
+                  <div className="box13"></div>
+                  <p>Cover Photos/Banners</p>
+                </div>
+              </div>
+
+              <div className="sect2-div1-box1">
+                <div className="sect2-div1-box1-data">
+                  <div className="box13"></div>
+                  <p>Highlights Covers</p>
+                </div>
+              </div>
+
+              <div className="sect2-div1-box1">
+                <div className="sect2-div1-box1-data">
+                  <div className="box13"></div>
+                  <p>YouTube Thumbnails</p>
+                </div>
+              </div>
+
+              <div className="sect2-div1-box1">
+                <div className="sect2-div1-box1-data">
+                  <div className="box13"></div>
+                  <p>Podcast Cover Art</p>
+                </div>
+              </div>
+
+              <div className="sect2-div1-box1">
+                <div className="sect2-div1-box1-data">
+                  <div className="box13"></div>
+                  <p>Memes</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* ------------------------------------------------------------- */}
+          {/* Section 4 */}
+          <div className="sect1">
+            <p> Print & Physical Graphics</p>
+
+            {/* sec1-di1 */}
+            <div className="sect1-div1">
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box14"></div>
+                  <p>Book Covers</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box14"></div>
+                  <p>Magazines</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box14"></div>
+                  <p>Newspaper Ads</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box14"></div>
+                  <p>Menus</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box14"></div>
+                  <p>Product Labels</p>
+                </div>
+              </div>
+            </div>
+
+            {/* Section4- div2 */}
+
+            <div className="sect1-div2">
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box14"></div>
+                  <p>Stickers</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box14"></div>
+                  <p>Postcards</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box14"></div>
+                  <p>Album or CD Covers</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box14"></div>
+                  <p>Tickets</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* ---------------------------------------------------------------- */}
+          {/* Section 5 */}
+          <div className="sect1">
+            <p>Event & Conference Graphics</p>
+
+            {/* Section5-di1 */}
+            <div className="sect1-div1">
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box15"></div>
+                  <p>Event Programs</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box15"></div>
+                  <p>Conference Badges</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box15"></div>
+                  <p>Stage Backdrops</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box15"></div>
+                  <p>Event Tickets</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box15"></div>
+                  <p>Roll-up Banners</p>
+                </div>
+              </div>
+            </div>
+
+            {/* Section5- div2 */}
+
+            <div className="sect1-div2">
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box15"></div>
+                  <p>Name Tags</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box15"></div>
+                  <p>Directional Signage</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* ---------------------------------------------------------------- */}
+          {/* Section 6 */}
+          <div className="sect1">
+            <p>Informational & Public Service Graphics</p>
+
+            {/* Section5-di1 */}
+            <div className="sect1-div1">
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box16"></div>
+                  <p>
+                    Posters for Awareness
+                    <br />
+                    Campaigns
+                  </p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box16"></div>
+                  <p>Government Notices</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box16"></div>
+                  <p>
+                    Health and Safety
+                    <br />
+                    Posters
+                  </p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box16"></div>
+                  <p>Traffic Signage</p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box16"></div>
+                  <p>
+                    Public Service
+                    <br />
+                    Announcements
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* Section4- div2 */}
+
+            <div className="sect1-div2">
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box16"></div>
+                  <p>
+                    Maps (event layouts,
+                    <br />
+                    city maps)
+                  </p>
+                </div>
+              </div>
+
+              <div className="sect1-div1-box1">
+                <div className="sect1-div1-box1-data">
+                  <div className="box16"></div>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* ---------------------------------------------------------------- */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default page
