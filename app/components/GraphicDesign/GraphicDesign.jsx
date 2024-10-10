8865e408731af2491d55dba68ec3226640c913df
neha-patankar <nehapatankar122@gmail.com>
2024-10-10 12:06:05 -0700

    make a graphics page

---
+++ app/components/GraphicDesign/GraphicDesign.jsx
@@ -0,0 +1,292 @@
+import styles from '../../../styles/GaphicsDesign/GraphicsDesign.module.css';
+
+const GraphicDesign = () => {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>
+          Create stunning graphic <br></br>designs effortlessly with AI
+        </h1>
+        <p className={styles.subtitle}>
+          Quickly craft designs that captivate and communicate your vision clearly.
+        </p>
+      </header>
+
+      <section className={styles.section}>
+        <div className={styles.headingRow}>
+          <h2 className={styles.sectionHeading}>Marketing & Promotional Graphics</h2>
+        </div>
+        <div className={styles.flexContainer}>
+          <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Blank Layout</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Flayers</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Brochures</h1>
+        </div>
+          
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Banners</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Social Media Posts</h1>
+        </div>
+        </div>
+
+        <div className={styles.flexContainer}>
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Advertisements</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Emain Campaign</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Event Invitations</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Coupons or Vouchers</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Blank Layout</h1>
+        </div>
+        </div>
+
+        <div className={styles.headingRow}>
+          <h2 className={styles.sectionHeading}>Branding & Corporate Identity Graphics</h2>
+        </div>
+        <div className={styles.flexContainer}>
+       
+
+        <div className={styles.card}>
+            <div className={styles.box2}></div>
+            <h1 className={styles.heading }>Business Cards</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box2}></div>
+            <h1 className={styles.heading }>Letterheads</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box2}></div>
+            <h1 className={styles.heading }>Employee ID Cards</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box2}></div>
+            <h1 className={styles.heading }>Press Kits</h1>
+        </div>
+     </div>
+
+
+     <div className={styles.headingRow}>
+          <h2 className={styles.sectionHeading}>Social Media & Digital Content Graphics</h2>
+        </div>
+        <div className={styles.flexContainer}>
+       
+
+        <div className={styles.card}>
+            <div className={styles.box2}></div>
+            <h1 className={styles.heading }>Business Cards</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box2}></div>
+            <h1 className={styles.heading }>Letterheads</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box2}></div>
+            <h1 className={styles.heading }>Employee ID Cards</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box2}></div>
+            <h1 className={styles.heading }>Press Kits</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box2}></div>
+            <h1 className={styles.heading }>Press Kits</h1>
+        </div>
+     </div>
+
+
+     <div className={styles.headingRow}>
+          <h2 className={styles.sectionHeading}> Print & Physical Graphics</h2>
+        </div>
+        <div className={styles.flexContainer}>
+          <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Blank Layout</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Flayers</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Brochures</h1>
+        </div>
+          
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Banners</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Social Media Posts</h1>
+        </div>
+        </div>
+
+        <div className={styles.flexContainer}>
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Advertisements</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Emain Campaign</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Event Invitations</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Coupons or Vouchers</h1>
+        </div>
+       </div>
+
+
+
+       <div className={styles.headingRow}>
+          <h2 className={styles.sectionHeading}> Event & Conference Graphics</h2>
+        </div>
+        <div className={styles.flexContainer}>
+          <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Blank Layout</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Flayers</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Brochures</h1>
+        </div>
+          
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Banners</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Social Media Posts</h1>
+        </div>
+        </div>
+
+        <div className={styles.flexContainer}>
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Advertisements</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Emain Campaign</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Event Invitations</h1>
+        </div>
+        </div>
+
+        
+        <div className={styles.headingRow}>
+          <h2 className={styles.sectionHeading}>Informational & Public Service Graphics </h2>
+        </div>
+        <div className={styles.flexContainer}>
+          <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Blank Layout</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Flayers</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Brochures</h1>
+        </div>
+          
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Banners</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Social Media Posts</h1>
+        </div>
+        </div>
+
+        <div className={styles.flexContainer}>
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Advertisements</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Emain Campaign</h1>
+        </div>
+
+        <div className={styles.card}>
+            <div className={styles.box}></div>
+            <h1 className={styles.heading }>Event Invitations</h1>
+        </div>
+        </div>
+
+      </section>
+    </div>
+  );
+};
+
+export default GraphicDesign;
--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import ToolsHomeSidebar from "./components/sidebars/ToolsHomeSidebar";
 import VideoCreationPage from "./components/sidebars/VideoCreations";
+import GraphicDesign from "./components/GraphicDesign/GraphicDesign";
 
 export default function Home() {
   return (
     <div>
-      <ToolsHomeSidebar/>
-       {/* <VideoCreationPage/> */}
+    {/*  <ToolsHomeSidebar/>
+      <VideoCreationPage/> */}
+      <GraphicDesign/>
     </div>
   );
 }
