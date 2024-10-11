34645366fe323130428a0f0dbe093152850eac1d
Hitanshi lalwani <hitanshi077@gmail.com>
2024-10-11 14:03:18 +0530

    Used Home component in page.js

--- app/page.js
+++ app/page.js
@@ -3,15 +3,11 @@ import VoiceOver from "./components/voiceOver/VoiceOver";
 import Workspace from "./components/voiceOver/Workspace";
 import MyComponent from "./components/voiceOver/MyComponent";
 import Home from "./components/homePage/Home";
-import Image from "next/image";
-import ToolsHomeSidebar from "./components/sidebars/ToolsHomeSidebar";
-import VideoCreationPage from "./components/sidebars/VideoCreations";
 
 const page = () => {
   return (
     <div>
-      {/* <ToolsHomeSidebar/> */}
-       <VideoCreationPage/>
+       <Home/>
     </div>
   );
 };
