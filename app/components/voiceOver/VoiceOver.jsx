cc077a38f48d5e382ae0fd70148887525e8903ef
shaninishad1999 <shaninishad1999@gmail.com>
2024-10-11 15:49:10 +0530

    minor fixes

--- app/components/voiceOver/VoiceOver.jsx
+++ app/components/voiceOver/VoiceOver.jsx
@@ -9,6 +9,7 @@ import Shape from "@/public/VoiceOver/shape.png";
 import arrow from "@/public/VoiceOver/arrow.png";
 import coins from "@/public/VoiceOver/coins.png";
 import crown from "@/public/VoiceOver/crown.png";
+
 const VoiceOver = () => {
   const samplePrompts = [
     "Create a modern portfolio website for a graphic designer with bold visuals and smooth navigation",
